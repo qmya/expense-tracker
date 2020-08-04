79592741448b3462bb34e4598ecb6f367efbda20
qmya <qandeelgul90@gmail.com>
2020-08-04 14:21:24 -0400

    added the income expenses to the app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Header from './components/Header/Header.js';
-import Balance from './components/Balance/Balance.js';
+import Header from './components/Header.js';
+import Balance from './components/Balance.js';
+import IncomeExpenses from './components/IncomeExpenses.js';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <div className="container">
         <Balance/>
+        <IncomeExpenses/>
       </div>
 
     </div>
